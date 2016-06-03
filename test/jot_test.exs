5385418a2dff773dfd55c2ac3cfe3993bb190f7e
Louis Pilfold <louis@lpil.uk>
2016-06-03 15:23:42 +0100

    Start integration tests

--- test/jot_test.exs
+++ test/jot_test.exs
@@ -2,7 +2,22 @@ defmodule JotTest do
   use ExUnit.Case
   doctest Jot
 
-  test "the truth" do
-    assert 1 + 1 == 2
+  defmacro template >>> output do
+    quote do
+      html     = unquote(template) |> Jot.eval_string
+      expected = unquote(output) |> String.replace("\n", "")
+      assert html == expected
+    end
+  end
+
+  test "eval_string/3" do
+    """
+    - x = " world!"
+    #greeting(class="big") Hello
+      i
+        = x
+    """ >>> """
+    <div id="greeting" class="big">Hello<i> world!</i></div>
+    """
   end
 end
