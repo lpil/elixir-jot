27ce294ca0f67e6f77afb9864b06b82235d3773e
Louis Pilfold <louis@lpil.uk>
2016-06-04 16:24:31 +0100

    Complete public API

--- lib/jot.ex
+++ lib/jot.ex
@@ -72,15 +72,105 @@ defmodule Jot do
 
 
   @doc """
+  Generates a function definition from the file contents.
+
+  The kind (`:def` or `:defp`) must be given, the
+  function name, its arguments and the compilation options.
+
+  This function is useful in case you have templates but
+  you want to precompile inside a module for speed.
+
+  ## Examples
+
+      iex> # sample.eex
+      ...> # <%= a + b %>
+      ...> defmodule SampleA do
+      ...>   require Jot
+      ...>   Jot.function_from_file :def, :sample, "sample.jot", [:a, :b]
+      ...> end
+      ...> SampleA.sample(1, 2)
+      "3"
+
+  """
+  defmacro function_from_file(kind, name, file, args \\ [], options \\ []) do
+    quote bind_quoted: binding do
+      info = Keyword.merge options, [file: file, line: 1]
+      args = Enum.map(args, fn arg -> {arg, [line: 1], nil} end)
+      compiled = Jot.compile_file(file, info)
+      @external_resource file
+      @file file
+      case kind do
+        :def  -> def(unquote(name)(unquote_splicing(args)), do: unquote(compiled))
+        :defp -> defp(unquote(name)(unquote_splicing(args)), do: unquote(compiled))
+      end
+    end
+  end
+
+
+  @doc """
+  Generates a function definition from the string.
+
+  The kind (`:def` or `:defp`) must be given, the
+  function name, its arguments and the compilation options.
+
+  ## Examples
+
+      iex> defmodule SampleB do
+      ...>   require Jot
+      ...>   Jot.function_from_string :def, :sample, "= a + b", [:a, :b]
+      ...> end
+      iex> SampleB.sample(1, 2)
+      "3"
+
+  """
+  defmacro function_from_string(
+    kind, name, source, args \\ [], options \\ []
+  ) do
+    quote bind_quoted: binding do
+      info = Keyword.merge([file: __ENV__.file, line: __ENV__.line], options)
+      args = Enum.map(args, fn(arg) -> {arg, [line: info[:line]], nil} end)
+      compiled = Jot.compile_string(source, info)
+      case kind do
+        :def  ->
+          def(unquote(name)(unquote_splicing(args)), do: unquote(compiled))
+        :defp ->
+          defp(unquote(name)(unquote_splicing(args)), do: unquote(compiled))
+      end
+    end
+  end
+
+
+  @doc """
   Takes a Jot template string and returns a quoted expression
   that can be evaluated by Elixir, or compiled to a function.
+
+  ## Examples
+
+    iex> expr = Jot.compile_string("= a + b")
+    ...> Jot.eval_quoted(expr, [a: 1, b: 2])
+    "3"
   """
   def compile_string(template, opts \\ []) when is_binary(template) do
     Jot.Compiler.compile(template, opts)
   end
 
+
+  @doc """
+  Takes a `path` to a Jot template file and generate a quoted
+  expression that can be evaluated by Elixir or compiled to a function.
+  """
+  def compile_file(path, options \\ []) do
+    options = Keyword.merge(options, [file: path, line: 1])
+    compile_string(File.read!(path), options)
+  end
+
+
   @doc """
   Takes a Jot template string and evaluate it using the `bindings`.
+
+  This function compiles the template with every call, making it considerably
+  slower than functions generated with `function_from_string/5` and
+  `function_from_file/5`.
 
   ## Examples
 
@@ -90,7 +180,15 @@ defmodule Jot do
   def eval_string(template, bindings \\ [], opts \\ []) do
     template
     |> compile_string(opts)
-    |> Code.eval_quoted(bindings)
-    |> elem(0)
+    |> eval_quoted(bindings)
+  end
+
+
+  @doc """
+  Takes a Jot quoted expression and call it using the `bindings` given.
+  """
+  def eval_quoted(expr, bindings \\ []) do
+    {html, _} = Code.eval_quoted(expr, bindings)
+    html
   end
 end
